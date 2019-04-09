978551aa1a3c5f51ba869755b5f4041b9f8c7dc7
KYIV\Igor_Lopushko <Igor_Lopushko@epam.com>
2019-04-09 15:34:08 +0300

    Create production like configuration and HTML pages structure

--- webui/webpack.prod.js
+++ webui/webpack.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackExternalsPlugin from 'html-webpack-externals-plugin';
 
 export default {
     mode: 'production',
@@ -21,7 +22,8 @@ export default {
         // Create HTML file that includes reference to bundled JS.
         new HtmlWebpackPlugin({
             template: 'src/index.html',
-            inject: true,
+            filename: 'index.html',
+            inject: 'head',
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
@@ -34,6 +36,36 @@ export default {
                 minifyCSS: true,
                 minifyURLs: true,
             },
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/about.html',
+            filename: 'about.html',
+            inject: 'head',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
+            },
+        }),
+        new HtmlWebpackExternalsPlugin({
+            externals: [
+                {
+                    module: 'jquery',
+                    entry: 'dist/jquery.min.js',
+                    global: 'jQuery',
+                },
+                {
+                    module: 'bootstrap',
+                    entry: ['dist/css/bootstrap.min.css', 'dist/js/bootstrap.min.js'],
+                },
+            ],
         }),
     ],
     module: {
